ff355cc561cfca34c6ae77fdaa3ab06529df21c9
Michael <shamanoff@gmail.com>
2016-08-29 22:13:12 +0300

    home

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 $(function() {
     $(document).foundation();
 });
+
+
+
+jQuery(document).ready(function(){
+    $objWindow = $(window);
+    $('div[data-type="background"]').each(function(){
+        var $bgObj = $(this);
+        $(window).scroll(function() {
+            var yPos = -($objWindow.scrollTop() / $bgObj.data('speed'));
+            var coords = '100% '+ yPos + 'px';
+            $bgObj.css({ backgroundPosition: coords });
+        });
+    });
+});
